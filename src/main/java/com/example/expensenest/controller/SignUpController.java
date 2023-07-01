23d8626182a9fbe2c7f797cf4e5a15238106d0c7
arey1124 <arihant0996@gmail.com>
2023-07-01 19:37:12 -0300

    Remove code related to session changes

--- src/main/java/com/example/expensenest/controller/SignUpController.java
+++ src/main/java/com/example/expensenest/controller/SignUpController.java
@@ -2,15 +2,11 @@ package com.example.expensenest.controller;
 
 import com.example.expensenest.entity.User;
 import com.example.expensenest.service.UserService;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-
-import java.net.HttpCookie;
 
 @Controller
 public class SignUpController {
@@ -22,20 +18,15 @@ public class SignUpController {
     }
 
     @GetMapping("/signup")
-    public String getSignUpPage (HttpServletRequest request, HttpSession session, Model model) {
+    public String getSignUpPage (Model model) {
         User user = new User();
         model.addAttribute("user", user);
-        if(session.getAttribute("loggedInUser") != null) {
-            // If user is logged in and session exists for the user then redirect to dashboard
-            System.out.println(((User) session.getAttribute("loggedInUser")).getName());
-        }
         return "signup";
     }
 
     @PostMapping("/user/create")
-    public String createUser(HttpServletRequest request, HttpSession session, @ModelAttribute("user") User user) {
+    public String createUser(@ModelAttribute("user") User user) {
         userService.addUser(user);
-        session.setAttribute("loggedInUser", user);
         // TODO: Need to redirect to password setup page
         return "redirect:/signup";
     }
