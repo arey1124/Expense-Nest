c2c4a012aeeb219a9f794d0e246373a9c894b653
Arihant Dugar <ar968345@dal.ca>
2023-07-20 10:59:49 -0300

    Manage categories and add categories

--- src/main/java/com/example/expensenest/entity/Category.java
+++ src/main/java/com/example/expensenest/entity/Category.java
@@ -1,9 +1,12 @@
 package com.example.expensenest.entity;
 
+import com.example.expensenest.enums.CategoryType;
+
 public class Category {
     private int id;
     private String name;
     private String image;
+    private int totalProducts;
 
     public int getId() {
         return id;
@@ -27,5 +30,38 @@ public class Category {
 
     public void setImage(String image) {
         this.image = image;
+    }
+
+    public int getTotalProducts() {
+        return totalProducts;
+    }
+
+    public void setTotalProducts(int totalProducts) {
+        this.totalProducts = totalProducts;
+    }
+
+    public String formatImageData(String typeImage) {
+        CategoryType result = null;
+        for (CategoryType category : CategoryType.values()) {
+            if (category.value.equalsIgnoreCase(typeImage)) {
+                result = category;
+                break;
+            }
+        }
+
+        switch(result) {
+            case ELECTRONICS:
+                return "/images/electronics.png";
+            case ELECTRONIC_ACCESSORIES:
+                return "/images/electronic-accessories.png";
+            case FRESH_FOOD_AND_GROCERY:
+                return "/images/food-grocery.png";
+            case FRUITS_AND_VEGETABLES:
+                return "/images/fruits.png";
+            case MEAT_AND_DAIRY:
+                return "/images/dairy.png";
+            default:
+                return null;
+        }
     }
 }
---
+++ src/main/java/com/example/expensenest/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,26 @@
+package com.example.expensenest.service.impl;
+
+import com.example.expensenest.entity.Category;
+import com.example.expensenest.repository.CategoryRepository;
+import com.example.expensenest.service.CategoryService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+    private CategoryRepository categoryRepository;
+    public CategoryServiceImpl (CategoryRepository categoryRepository) {
+        super();
+        this.categoryRepository = categoryRepository;
+    }
+
+    public boolean addCategory(Category category) {
+        return categoryRepository.addCategory(category);
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.getAllCategories();
+    }
+}
