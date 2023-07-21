30835237b377a468e1d3ffbe1eb6fb691c6342f7
Arihant Dugar <ar968345@dal.ca>
2023-07-20 21:42:34 -0300

    Display products by categories

---
+++ src/main/java/com/example/expensenest/service/impl/ProductServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.expensenest.service.impl;
+
+import com.example.expensenest.entity.Products;
+import com.example.expensenest.repository.ProductRepository;
+import com.example.expensenest.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private ProductRepository productRepository;
+    public ProductServiceImpl (ProductRepository productRepository) {
+        super();
+        this.productRepository = productRepository;
+    }
+    public List<Products> getProductsByCategory(int categoryId) {
+        return productRepository.getProductsByCategory(categoryId);
+    }
+}
