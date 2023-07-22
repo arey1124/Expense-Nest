912ab190d802d2a6b8f9d133699e006d11692e4e
Kainatkhan89 <kainatkhan892014@gmail.com>
2023-07-22 10:38:03 -0300

    Merged changes in customer edit profile

--- src/main/java/com/example/expensenest/service/UserService.java
+++ src/main/java/com/example/expensenest/service/UserService.java
@@ -14,9 +14,7 @@ public interface UserService {
 
     boolean verifyUser(String code);
 
-    boolean editUser(User user);
-
-    User getUserProfile(int UserId);
+    boolean updateUser(User user);
 
     User getUserByEmailAndPassword(UserSignIn userSignIn);
 
