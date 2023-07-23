9b88116e5b8e81fd9cb59bbafd1f8bb71a2656aa
Jinal Dave <jn270099@dal.ca>
2023-07-23 00:09:55 -0300

    Implement logging for Product Insights feature

--- src/main/java/com/example/expensenest/controller/ProductInsightsController.java
+++ src/main/java/com/example/expensenest/controller/ProductInsightsController.java
@@ -7,6 +7,8 @@ import com.example.expensenest.service.UserInsightsService;
 import com.example.expensenest.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProductInsightsController {
+    private static final Logger logger = LogManager.getLogger(ProductInsightsController.class);
+
     private SessionService sessionService;
     private UserService userService;
     private UserInsightsService userInsightsService;
@@ -26,17 +30,27 @@ public class ProductInsightsController {
 
     @GetMapping("/productInsights")
     public String getSellerEditProfile() {
+        logger.info("Handling GET request for /productInsights");
         return "/productInsights";
     }
 
     @GetMapping("/seller-chart-data")
     @ResponseBody
-    public UserInsightResponse getProductInsights(HttpServletRequest request){
+    public UserInsightResponse getProductInsights(HttpServletRequest request) {
+        logger.info("Handling GET request for /seller-chart-data");
+
         HttpSession session = request.getSession();
         User userSession = sessionService.getSession(session);
+        logger.debug("User session data: {}", userSession);
+
         User profile = userService.getUserProfile(userSession.getId());
-        var response =  userInsightsService.getUserInsightResponse(profile.getId());
+        logger.info("Fetching user profile for userId: {}", userSession.getId());
+        logger.debug("User profile data: {}", profile);
+
+        UserInsightResponse response = userInsightsService.getUserInsightResponse(profile.getId());
+        logger.info("Fetching product insights for userId: {}", profile.getId());
+        logger.debug("Product insights data: {}", response);
+
         return response;
     }
-
 }
