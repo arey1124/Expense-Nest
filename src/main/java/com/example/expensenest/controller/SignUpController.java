a5cf58c9048a5d942455ea52f10836c9bb5f6de4
arey1124 <arihant0996@gmail.com>
2023-07-26 11:42:09 -0300

    Added logging for controllers and service calls

--- src/main/java/com/example/expensenest/controller/SignUpController.java
+++ src/main/java/com/example/expensenest/controller/SignUpController.java
@@ -3,6 +3,8 @@ package com.example.expensenest.controller;
 import com.example.expensenest.entity.User;
 import com.example.expensenest.service.EmailSenderService;
 import com.example.expensenest.service.UserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class SignUpController {
 
+    private static final Logger logger = LogManager.getLogger(SignUpController.class);
     private UserService userService;
-private  final EmailSenderService emailSenderService;
+    private  final EmailSenderService emailSenderService;
+
     public SignUpController(UserService userService, EmailSenderService emailSenderService) {
         this.userService = userService;
         this.emailSenderService = emailSenderService;
@@ -22,6 +26,7 @@ private  final EmailSenderService emailSenderService;
 
     @GetMapping("/signup")
     public String getSignUpPage (Model model) {
+        logger.info("Handling GET request for /signup");
         User user = new User();
         model.addAttribute("user", user);
         return "signup";
@@ -29,11 +34,14 @@ private  final EmailSenderService emailSenderService;
 
     @PostMapping("/user/create")
     public String createUser(@ModelAttribute("user") User user, Model model) {
+        logger.info("Handling POST request for /user/create");
         String code = userService.addUser(user);
         if (code == null) {
+            logger.info("Handling user exists condition");
             model.addAttribute("userExists",true);
             return "/signup";
         } else {
+            logger.info("Sending email for user verification");
             emailSenderService.sendVerificationEmail(user.getEmail(),"Please verify your email","Click the following link to verify your email", code);
             model.addAttribute("user",user);
             return "/signup";
--- src/main/java/com/example/expensenest/service/impl/InvoiceServiceImpl.java
+++ src/main/java/com/example/expensenest/service/impl/InvoiceServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.expensenest.service.impl;
 import com.example.expensenest.entity.Invoice;
 import com.example.expensenest.repository.InvoiceRepository;
 import com.example.expensenest.service.InvoiceService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.List;
 @Service
 public class InvoiceServiceImpl implements InvoiceService {
 
+    private static final Logger logger = LogManager.getLogger(InvoiceServiceImpl.class);
     private InvoiceRepository invoiceRepository;
     public InvoiceServiceImpl (InvoiceRepository invoiceRepository) {
         super();
@@ -18,16 +21,19 @@ public class InvoiceServiceImpl implements InvoiceService {
 
     @Override
     public List<Invoice> getUserInvoices(int userId) {
+        logger.info("Fetching all invoices by userId : {}" , userId);
         return invoiceRepository.findAllUserInvoices(userId);
     }
 
     @Override
     public List<Invoice> getFilteredInvoices(int userId, String searchString) {
+        logger.info("Fetching all invoices by userId : {} and search query : {}" , userId, searchString);
         return invoiceRepository.getAllUserFilteredInvoices(userId, searchString);
     }
 
     @Override
     public boolean updateInvoiceArchiveData (int invoiceId, boolean isArchived, String archiveReason) {
+        logger.info("Updating invoices by invoiceId : {}" , invoiceId);
         return invoiceRepository.updateInvoiceArchiveData(invoiceId, isArchived, archiveReason);
     }
 }
