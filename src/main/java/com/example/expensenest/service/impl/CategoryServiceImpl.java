631dd62ddb61ee47f0a74d36634eb7f88a90a5a3
Arihant Dugar <ar968345@dal.ca>
2023-07-26 11:42:09 -0300

    Added logging for controllers and service calls

--- src/main/java/com/example/expensenest/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/example/expensenest/service/impl/CategoryServiceImpl.java
@@ -3,6 +3,8 @@ package com.example.expensenest.service.impl;
 import com.example.expensenest.entity.Category;
 import com.example.expensenest.repository.CategoryRepository;
 import com.example.expensenest.service.CategoryService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -10,6 +12,7 @@ import java.util.List;
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
+    private static final Logger logger = LogManager.getLogger(CategoryServiceImpl.class);
     private CategoryRepository categoryRepository;
     public CategoryServiceImpl (CategoryRepository categoryRepository) {
         super();
@@ -17,14 +20,17 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     public boolean addCategory(Category category) {
+        logger.info("Adding category");
         return categoryRepository.addCategory(category);
     }
 
     public Category getCategoryById(int categoryId) {
+        logger.info("Fetching category by id : {}" , categoryId);
         return categoryRepository.getCategoryById(categoryId);
     }
 
     public List<Category> getAllCategories() {
+        logger.info("Fetching all categories");
         return categoryRepository.getAllCategories();
     }
 }
